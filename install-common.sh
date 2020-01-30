041f14ef8a16e969f0d7372b0b8f0995ab3b7d1d
Yury Garris <ygarris@gmail.com>
2020-01-30 16:36:58 +0100

    moved installation of entertinement packages to separate file; added installation of dnscrypt-proxy

--- install-common.sh
+++ install-common.sh
@@ -8,6 +8,10 @@ if [[ $HOSTNAME != $(hostname) ]]; then
     sudo sed -i "s/127\.0\.1\.1\s.*/127\.0\.1\.1\t${HOSTNAME}/" /etc/hosts
 fi
 
+# Decrease swapiness
+echo 'vm.swappiness = 10' | sudo tee /etc/sysctl.d/swapiness.conf
+sudo sysctl -p --system
+
 # update packages
 sudo apt -y update
 sudo apt -y upgrade
@@ -61,3 +65,37 @@ sudo apt -y autoremove
 # disable KDE Baloo indexer
 balooctl disable
 balooctl purge
+
+# install cloudflared for DNS over HTTPS
+wget https://bin.equinox.io/c/VdrWdbjqyF/cloudflared-stable-linux-amd64.deb
+sudo dpkg -i cloudflared-stable-linux-amd64.deb || sudo apt -yf install
+rm cloudflared-stable-linux-amd64.deb
+sudo mkdir -p /usr/local/etc/cloudflared
+sudo cat << EOF > /usr/local/etc/cloudflared/config.yml
+proxy-dns: true
+proxy-dns-upstream:
+ - https://1.1.1.1/dns-query
+ - https://1.0.0.1/dns-query
+EOF
+sudo cloudflared service install
+
+
+# dns proxy
+# https://github.com/DNSCrypt/dnscrypt-proxy/wiki/Installation-linux
+# CHECK apt install dnscrypt-proxy
+mkdir -p ~/bin/dns-proxy
+curl -L "https://github.com/DNSCrypt/dnscrypt-proxy/releases/download/2.0.36/dnscrypt-proxy-linux_x86_64-2.0.36.tar.gz" | tar -zxv --strip-components=1 -C ~/bin/dns-proxy
+cp ~/bin/dns-proxy/example-dnscrypt-proxy.toml ~/bin/dns-proxy/dnscrypt-proxy.toml
+## server_names = ['scaleway-fr', 'google', 'yandex', 'cloudflare']
+# server_names = ['cloudflare', 'cloudflare-ipv6']
+## listen_addresses = ['127.0.0.1:53']
+# listen_addresses = ['127.0.0.1:53', '[::1]:53']
+sudo systemctl stop systemd-resolved
+sudo systemctl disable systemd-resolved
+sudo apt -y remove resolvconf
+sudo cp /etc/resolv.conf /etc/resolv.conf.backup
+sudo rm -f /etc/resolv.conf
+echo "
+nameserver 127.0.0.1
+options edns0
+" | sudo tee /etc/resolv.conf
---
+++ install-entertainment.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# install packages
+sudo apt -y install \
+    steam \
+    vlc \
+    streamripper
