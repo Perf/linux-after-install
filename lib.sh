9dac0786188f920b9e09ead5960d87534c56bcb4
Yury Garris <y.garris@sevensenders.com>
2022-11-23 16:33:58 +0100

    switched installing of docker compose from standalone to using repo

--- lib.sh
+++ lib.sh
@@ -217,10 +217,12 @@ function install_docker_and_docker_compose() {
       read -p "Docker and Docker Compose [<Enter>|any key]: " answer
       case ${answer} in
           '' )  printf ">> Installing Docker and Docker Compose\n"
-                sudo apt -y install docker.io
-                local DOCKER_COMPOSE_VERSION=$(curl --silent 'https://api.github.com/repos/docker/compose/releases/latest' | jq '.tag_name' -r)
-                sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
-                sudo chmod +x /usr/local/bin/docker-compose
+                sudo apt -y install ca-certificates curl gnupg lsb-release
+                sudo mkdir -p /etc/apt/keyrings
+                curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+                echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+                sudo apt update
+                sudo apt -y install docker.io docker-compose-plugin
                 sudo usermod -a -G docker ${USER}
                 break;;
 
