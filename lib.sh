fdbc06bb796d977f38f195be7f17409c4d4063c8
Yury Garris <Perf@users.noreply.github.com>
2023-09-25 15:58:31 +0200

    Update lib.sh
    Added installation of a docker buildx plugin

--- lib.sh
+++ lib.sh
@@ -235,7 +235,7 @@ function install_docker_and_docker_compose() {
                 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
                 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
                 sudo apt update
-                sudo apt -y install docker.io docker-compose-plugin
+                sudo apt -y install docker.io docker-compose-plugin docker-buildx-plugin
                 sudo usermod -a -G docker ${USER}
                 break;;
 
